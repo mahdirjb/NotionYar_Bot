d0e415978cb3d39901940b42c82dd5523b6b41b9
Mahdi Rajabzadeh <irmr81@gmail.com>
2026-09-06 19:05:40 +0330

    fix(states): add waiting_for_gratitude_item and quick_run_active states

--- app/states/habit_state.py
+++ app/states/habit_state.py
@@ -6,3 +6,5 @@ from aiogram.fsm.state import State, StatesGroup
 class HabitState(StatesGroup):
     waiting_for_custom_date = State()
     waiting_for_notes = State()
+    waiting_for_gratitude_item = State()
+    quick_run_active = State()
